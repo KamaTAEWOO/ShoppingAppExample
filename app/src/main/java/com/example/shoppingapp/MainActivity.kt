4eba85999d18291343725ee4f42a28eaaea8b535
KamaTAEWOO <zszs2020@naver.com>
2022-09-02 10:56:42 +0900

    1. 장바구니 UI Test 완료 2. 기능 구현 시작

--- app/src/main/java/com/example/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppingapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.shoppingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.shoppingapp.User.Login
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, Login::class.java)) //intent와 같음!
+            finish()//캘린더화면은 종료한다!
+        }, 2000)
     }
 }
